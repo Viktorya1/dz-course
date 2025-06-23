5a1150c5c95c7e9a656194536c8b0f2f6325a873
Viktoria <victoryaaam@mail.ru>
2025-06-23 17:24:06 +0300

    update first homework

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
-<script setup></script>
+<script setup>
+let data = new Date();
+
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let date = data.getDate();
+let nowMonth = data.getMonth();
+let year = data.getFullYear();
+</script>
 
 <template>
-  <h2>23.06.2025</h2>
+  <h2>{{ date }} {{ months[Number(nowMonth)] }} {{ year }}</h2>
 </template>
 
 <style scoped></style>

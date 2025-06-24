8a9dec50fc9753a76e14da848ebf7ee114c34b28
Viktoria <victoryaaam@mail.ru>
2025-06-24 22:40:13 +0300

    DZ 3

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
 <script setup>
 import Button from "./components/Button.vue";
+import Score from "./components/Score.vue";
+import Card from "./components/Card.vue";
+const score = 100;
+
+function getSelect(status) {
+  console.log(status);
+}
 </script>
 
 <template>
+  <header class="header">
+    <p class="header-text">Запомни слово</p>
+    <Score :score="score" />
+  </header>
   <main class="main">
-    <Button>Начать игру</Button>
+    <Button class="start-btn">Начать игру</Button>
   </main>
+  <Card @reverse-card="getSelect" @select-card="getSelect" />
 </template>
 
-<style scoped></style>
+<style scoped>
+.main {
+  text-align: center;
+  margin-top: 30vh;
+}
+
+.header {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 50px;
+}
+
+.header-text {
+  font-family: var(--font);
+  font-size: 16px;
+  font-weight: 700;
+  text-transform: uppercase;
+}
+</style>
---
+++ src/components/Score.vue
@@ -0,0 +1,35 @@
+<script setup>
+import IconLike from "../icons/IconLike.vue";
+
+const { score } = defineProps({
+  score: String,
+});
+</script>
+
+<template>
+  <div class="header-btn">
+    <p>{{ score }}</p>
+    <IconLike />
+  </div>
+</template>
+
+<style scoped>
+.header-btn {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  background: var(--color-btn-light);
+  color: var(--color-btn);
+  font-size: 16px;
+  font-family: var(--font);
+  font-weight: 700;
+  border-radius: 36px;
+  padding: 12px 24px;
+  box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.15);
+}
+
+p {
+  margin: 0;
+  padding: 0;
+}
+</style>
